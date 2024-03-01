34573ad5e79cd0eb138cfaba070dbda97a31893d
philaurin <philippe.laurin4@gmail.com>
2024-02-29 23:58:51 -0500

    stopshooter

---
+++ src/main/java/frc/robot/commands/shooter/StopShooter.java
@@ -0,0 +1,29 @@
+package frc.robot.commands.shooter;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Shooter;
+
+public class StopShooter extends Command {
+    private final Shooter m_shooter;
+
+    public StopShooter(Shooter shooter) {
+        m_shooter = shooter;
+        addRequirements(shooter);
+    }
+
+    @Override
+    public void execute() {
+        m_shooter.roll(0;);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        System.out.println(getClass().getSimpleName() + ".end() interrupted: " + interrupted);
+        m_shooter.roll(0);
+    }
+}
